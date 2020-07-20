6b6c9686204c2eebed0bdb18e0b7f88766407ad2
阮帅 <524007333@qq.com>
2020-07-20 23:48:21 +0800

    done 36

---
+++ src/com/ruanshuai/_30/Solution.java
@@ -0,0 +1,22 @@
+package com.ruanshuai._30;
+
+import java.util.*;
+
+/**
+ * @author ruanshuai
+ * @date 2020/7/20
+ */
+
+public class Solution {
+    public int FindGreatestSumOfSubArray(int[] array) {
+        if (array == null || array.length == 0)
+            return 0;
+        int max = array[0];
+        int res = array[0];
+        for (int i = 1; i < array.length; i++) {
+            max = Math.max(max + array[i], array[i]);
+            res = Math.max(max, res);
+        }
+        return res;
+    }
+}
--- src/com/ruanshuai/_32/Solution.java
+++ src/com/ruanshuai/_32/Solution.java
@@ -2,35 +2,37 @@ package com.ruanshuai._32;
 
 /**
  * @author ruanshuai
- * @date 2020/7/5
+ * @date 2020/7/20
  */
 
 public class Solution {
-
-    public int GetUglyNumber_Solution(int index) {
-        if (index < 1)
-            return 0;
-        int[] array = new int[index];
-        array[0] = 1;
-        int index2 = 0;
-        int index3 = 0;
-        int index5 = 0;
-        for (int i = 1; i < index; i++) {
-            int min = min(array[index2] * 2, array[index3] * 3, array[index5] * 5);
-            array[i] = min;
-            while (array[i] == array[index2] * 2)
-                index2++;
-            while (array[i] == array[index3] * 3)
-                index3++;
-            while (array[i] == array[index5] * 5)
-                index5++;
+    public String PrintMinNumber(int[] numbers) {
+        if (numbers == null || numbers.length == 0)
+            return "";
+        int n = numbers.length;
+        String[] strings = new String[n];
+        for (int i = 0; i < n; i++) {
+            strings[i] = String.valueOf(numbers[i]);
         }
-        return array[index - 1];
+        sort(strings);
+        StringBuilder sb = new StringBuilder();
+        for (String str : strings) {
+            sb.append(str);
+        }
+        return sb.toString();
     }
 
-    public int min(int a, int b, int c) {
-        int res = Math.min(a, b);
-        return Math.min(res, c);
+    public void sort(String[] strings) {
+        int n = strings.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if ((strings[j] + strings[j + 1]).compareTo(strings[j + 1] + strings[j]) > 0) {
+                    String temp = strings[j];
+                    strings[j] = strings[j + 1];
+                    strings[j + 1] = temp;
+                }
+            }
+        }
     }
 
 }
