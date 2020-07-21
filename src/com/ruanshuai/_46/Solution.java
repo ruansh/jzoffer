70ecb31c4cfd6337bcccefe0a00e0dfda5db78df
阮帅 <ruanshuai_sx@qiyi.com>
2020-07-21 23:17:58 +0800

    done 45

---
+++ src/com/ruanshuai/_46/Solution.java
@@ -0,0 +1,12 @@
+package com.ruanshuai._46;
+
+/**
+ * @author ruanshuai
+ * @date 2020/7/21
+ */
+
+public class Solution {
+    public int LastRemaining_Solution(int n, int m) {
+        
+    }
+}
