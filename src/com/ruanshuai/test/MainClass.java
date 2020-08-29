232c017f3e6141469481428a6509a1a9d75848d3
阮帅 <524007333@qq.com>
2020-08-29 23:17:29 +0800

    add new

---
+++ src/com/ruanshuai/test/MainClass.java
@@ -0,0 +1,29 @@
+package com.ruanshuai.test;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+/**
+ * @author ruanshuai
+ * @date 2020/8/28
+ */
+
+public class MainClass {
+
+    public static void main(String[] args) {
+        try {
+            Class a = TestClass.class;
+            Object b = a.newInstance();
+            Field field = a.getDeclaredField("b");
+            Method method = a.getDeclaredMethod("func", String.class);
+//            System.out.println(field);
+//            System.out.println(method);
+            Object o = a.getDeclaredMethod("func", String.class);
+            System.out.println(method.invoke(b, "hello"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
